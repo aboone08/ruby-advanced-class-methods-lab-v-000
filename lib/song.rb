c2844c90d0169f4dea697fe234ee7425c714ff57
Andranique Boone <andranique.boone@gmail.com>
2019-04-11 01:19:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   end
 
   def self.new_from_filename(filename) #class constructor that accepts an .mp3 file
-    song.name = filename.split("-")
+    song_name = filename.split("-")
     artist_name = filename.delete(".mp3")
   end
 end
